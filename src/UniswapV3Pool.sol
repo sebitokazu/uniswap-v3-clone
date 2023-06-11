556cd6d3d0e09e6a0b7e0db8b42b1678b36f658e
Sebastian Itokazu <sebitokazu@gmail.com>
2023-06-11 20:39:50 -0300

    initial setup of uniswapv3 pool

---
+++ src/UniswapV3Pool.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.19;
+
+import "./lib/Tick.sol";
+import "./lib/Position.sol";
+
+contract UniswapV3Pool {
+    using Tick for mapping(int24 => Tick.Info);
+    using Position for mapping(bytes32 => Position.Info);
+    using Position for Position.Info;
+
+    int24 internal constant MIN_TICK = -887272;
+    int24 internal constant MAX_TICK = -MIN_TICK;
+
+    // Pool tokens
+    address public immutable token0;
+    address public immutable token1;
+
+    // Packing vars read together saves gas
+    struct Slot0 {
+        // Current sqrt(P)
+        uint160 sqrtPriceX96;
+        // Current tick
+        int24 tick;
+    }
+    Slot0 public slot0;
+
+    // Amount of liquidity L
+    uint128 public liquidity;
+
+    // Ticks info
+    mapping(int24 => Tick.Info) public ticks;
+    // Positions info
+    mapping(bytes32 => Position.Info) public positions;
+
+    
+    constructor(address _token0, address _token1, uint160 _sqrtPriceX96, int24 _tick) {
+        token0 = _token0;
+        token1 = _token1;
+        slot0 = Slot0({
+            sqrtPriceX96: _sqrtPriceX96,
+            tick: _tick
+        });
+    }
+}
---
+++ src/lib/Tick.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED 
+pragma solidity 0.8.19;
+
+library Tick {
+    struct Info {
+        bool initialized;
+        uint128 liquidity;
+    }
+}
